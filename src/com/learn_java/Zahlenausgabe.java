af5f33b40a63b3d3bf26115bfd3a5e6cf5553c41
je-boska <joneirikboska@gmail.com>
2025-05-03 16:56:43 +0200

    Add while and do/while loops

--- src/com/learn_java/Zahlenausgabe.java
+++ src/com/learn_java/Zahlenausgabe.java
@@ -11,5 +11,29 @@ public class Zahlenausgabe {
                 System.out.println();
             }
         }
+
+        int i = 1;
+        while (i <= 100) {
+            if (i < 10) System.out.print("    ");
+            if (i >= 10 && i < 100) System.out.print("   ");
+            if (i == 100)  System.out.print("  ");
+            System.out.print(i);
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+            i++;
+        }
+
+        int j = 1;
+        do {
+            if (j < 10) System.out.print("    ");
+            if (j >= 10 && j < 100) System.out.print("   ");
+            if (j == 100)  System.out.print("  ");
+            System.out.print(j);
+            if (j % 10 == 0) {
+                System.out.println();
+            }
+            j++;
+        } while (j <=100);
     }
 }
