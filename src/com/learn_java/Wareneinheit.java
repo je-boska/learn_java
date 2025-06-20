9b54c2c0357650be34feeb2ec094835cc25fa168
je-boska <joneirikboska@gmail.com>
2025-06-20 12:39:11 +0200

    Create Wareneinheit class with constructor, getters and three utility methods

---
+++ src/com/learn_java/Wareneinheit.java
@@ -0,0 +1,73 @@
+package com.learn_java;
+
+public class Wareneinheit {
+    private int aktuelleMenge;
+    private int maxMenge;
+    private int anzahlTransaktionen;
+    private boolean aktivStatus;
+
+    public Wareneinheit() {
+        this.aktuelleMenge = 0;
+        this.maxMenge = 0;
+        this.anzahlTransaktionen = 0;
+        this.aktivStatus = false;
+    }
+
+    public Wareneinheit(int startMenge) {
+        if (startMenge < 0) {
+            this.aktuelleMenge = startMenge;
+            this.maxMenge = startMenge;
+            this.anzahlTransaktionen = 1;
+            aktivStatus = true;
+        } else {
+            this.aktuelleMenge = 0;
+            this.maxMenge = 0;
+            this.anzahlTransaktionen = 0;
+            aktivStatus = false;
+        }
+    }
+
+    public int getAktuelleMenge() {
+        return aktuelleMenge;
+    }
+
+    public int getMaxMenge() {
+        return maxMenge;
+    }
+
+    public int getAnzahlTransaktionen() {
+        return anzahlTransaktionen;
+    }
+
+    public boolean getAktivStatus() {
+        return aktivStatus;
+    }
+
+    public void aktualisiereMenge(int neueMenge) {
+        anzahlTransaktionen++;
+        if (neueMenge > 0) {
+            aktuelleMenge = neueMenge;
+            if (aktuelleMenge > maxMenge) {
+                maxMenge = aktuelleMenge;
+            }
+            aktivStatus = true;
+        } else {
+            aktuelleMenge = 0;
+            aktivStatus = false;
+        }
+    }
+
+    public void resetWareneinheit() {
+        anzahlTransaktionen = 0;
+        aktuelleMenge = 0;
+        maxMenge = 0;
+        aktivStatus = false;
+    }
+
+    public void einheitAusgeben() {
+        System.out.println("Aktuelle Menge: " + aktuelleMenge);
+        System.out.println("Maximale Menge: " + maxMenge);
+        System.out.println("Anzahl der Transaktionen: " + anzahlTransaktionen);
+        System.out.println("Aktivstatus: " + aktivStatus);
+    }
+}
